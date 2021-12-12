2a0e6391692f3e193023426f16b02c4336edc62d
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-12-12 17:52:36 -0600

    As a USER I can VIEW A LIST OF SERVICES

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Search4Support.Data;
+using Search4Support.Models;
+using Search4Support.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,41 +12,88 @@ namespace Search4Support.Controllers
 {
     public class ServicesController : Controller
     {
-        // GET: ServicesController
-        public ActionResult Index()
+        private ServiceDbContext context;
+        public ServicesController(ServiceDbContext dbContext)
         {
-            return View();
+            context = dbContext;
+        }
+        // GET: ServicesController
+        public IActionResult Index()
+        {
+            List<Service> services = context.Services.ToList();
+            return View(services);
         }
 
         // GET: ServicesController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: ServicesController/Create
-        public ActionResult Create()
+        // GET: ServicesController/Add
+        public IActionResult Add()
         {
-            return View();
+            AddServiceViewModel addServiceViewModel = new AddServiceViewModel();
+            return View(addServiceViewModel);
         }
 
-        // POST: ServicesController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Add(AddServiceViewModel addServiceViewModel)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                Service theService = new Service
+                {
+                    Name = addServiceViewModel.Name,
+                    Description = addServiceViewModel.Description
+                };
+
+                context.Services.Add(theService);
+                context.SaveChanges();
+
+                return Redirect("/Services");
             }
-            catch
-            {
-                return View();
-            }
+
+            return View("Add", addServiceViewModel);
         }
+
+        public IActionResult Delete()
+        {
+            List<Service> services = context.Services.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] serviceIds)
+        {
+            foreach (int serviceId in serviceIds)
+            {
+                Service theService = context.Services.Find(serviceId);
+                context.Services.Remove(theService);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/Services");
+        }
+
+        /*        // POST: ServicesController/Add
+                [HttpPost]
+                [ValidateAntiForgeryToken]
+                public IActionResult Add(IFormCollection collection)
+                {
+                    try
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch
+                    {
+                        return View();
+                    }
+                }*/
 
         // GET: ServicesController/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int id)
         {
             return View();
         }
@@ -51,7 +101,7 @@ namespace Search4Support.Controllers
         // POST: ServicesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(int id, IFormCollection collection)
         {
             try
             {
@@ -64,7 +114,7 @@ namespace Search4Support.Controllers
         }
 
         // GET: ServicesController/Delete/5
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int id)
         {
             return View();
         }
@@ -72,7 +122,7 @@ namespace Search4Support.Controllers
         // POST: ServicesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
@@ -82,6 +132,15 @@ namespace Search4Support.Controllers
             {
                 return View();
             }
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Service theService = context.Services
+                .Single(s => s.Id == id);
+
+            ServiceDetailViewModel viewModel = new ServiceDetailViewModel(theService);
+            return View(viewModel);
         }
     }
 }
