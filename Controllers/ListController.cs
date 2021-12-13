90c2bf252f7d740c0b17640d050f444c6b0f2eaf
samwed78 <84482279+samwed78@users.noreply.github.com>
2021-12-12 18:42:56 -0600

    ^^

---
+++ Controllers/ListController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Search4Support.Data;
+using Search4Support.Models;
+using Search4Support.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Search4Support.Controllers
+{
+    public class ListController : Controller
+    {
+        internal static Dictionary<string, string> ColumnChoices = new Dictionary<string, string>()
+        {
+            {"all", "All" },
+            {"provider", "Provider" },
+            {"category", "Category"},
+            {"location", "Location" }
+        };
+
+        internal static List<string> TableChoices = new List<string>()
+        {
+            "provider",
+            "category",
+            "location"
+        };
+
+        private ServiceDbContext context;
+
+        public ListController(ServiceDbContext dbcontext)
+        {
+            context = dbcontext;
+        }
+
+        // GET:<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.columns = ColumnChoices;
+            ViewBag.tablechoices = TableChoices;
+            ViewBag.providers = context.Providers.ToList();
+            ViewBag.services = context.Services.ToList();
+            ViewBag.categories = context.Categories.ToList();
+            ViewBag.locations = context.Locations.ToList();
+            return View();
+        }
+
+
+        // list services by column and value
+        public IActionResult Services(string column, string value)
+        {
+            List<Service> services = new List<Service>();
+            List<ServiceDetailViewModel> displayServices = new List<ServiceDetailViewModel>();
+
+            if (column.ToLower().Equals("all"))
+            {
+                services = context.Services
+                    .Include(s => s.Provider)
+                    .ToList();
+
+                ViewBag.title = "All Services";
+            }
+            else
+            {
+                if (column == "provider")
+                {
+                    services = context.Services
+                        .Include(s => s.Provider)
+                        .Where(s => s.Provider.Name == value)
+                        .ToList();
+                }
+
+                else if (column == "category")
+                {
+                    services = context.Services
+                        .Include(s => s.Category)
+                        .Where(s => s.Category.Name == value)
+                        .ToList();
+                }
+
+                else if (column == "location")
+                {
+                    services = context.Services
+                        .Include(s => s.Location)
+                        .Where(s => s.Location.Address == value)
+                        .ToList();
+                }
+
+                ViewBag.title = "Services with " + ColumnChoices[column] + ": " + value;
+            }
+            ViewBag.services = displayServices;
+
+            return View();
+        }
+    }
+}
---
+++ Data/ServiceDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Search4Support.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Search4Support.Data
+{
+    public class ServiceDbContext : DbContext
+    {
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public ServiceDbContext(DbContextOptions<ServiceDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Service>();
+        }
+    }
+}
