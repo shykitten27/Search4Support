6e60de9a580cdbbd39c504f7885bd50b067de373
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-12-14 13:28:14 -0600

    service list view model

--- ViewModels/ServiceListViewModel.cs
+++ ViewModels/ServiceListViewModel.cs
@@ -1,25 +1,27 @@
 ﻿using Search4Support.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Search4Support.ViewModels
 {
-    public class ServiceDetailViewModel
+    public class ServiceListViewModel
     {
         public int ServiceId { get; set; }
         public string Name { get; set; }
         public string ProviderName { get; set; }
         public string CategoryName { get; set; }
         public string Location { get; set; }
-        public string Description { get; set; }
 
-        public ServiceDetailViewModel(Service theService)
+        public ServiceListViewModel(Service theService)
         {
             ServiceId = theService.Id;
             Name = theService.Name;
             ProviderName = theService.Provider.Name;
             CategoryName = theService.Category.Name;
             Location = theService.Location.Address;
-            Description = theService.Description;
+            
         }
     }
 }
