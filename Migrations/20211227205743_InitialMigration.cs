a40d374daebb57def2c8b2f7edf7bb687be5011f
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-12-27 15:08:34 -0600

    Provider Detail is functional

---
+++ Migrations/20211227205743_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Search4Support.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Providers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Providers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Providers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Providers");
+        }
+    }
+}
