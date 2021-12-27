3e9d68f0c656e85b39abb44eb83ceaaa70a89af0
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-12-27 14:57:10 -0600

    Providers List and Detail

--- Models/Provider.cs
+++ Models/Provider.cs
@@ -9,8 +9,9 @@ namespace Search4Support.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        //public string Description {get; set;}
+        public string Description {get; set;}
        
         //one:many Provider:Services
         public List<Service> Services { get; set; }
@@ -19,10 +20,12 @@ namespace Search4Support.Models
         public Provider()
         {
         }
-        public Provider(string name, string address)
+        public Provider(string name, string phoneNumber, string address, string description)
         {
             Name = name;
+            PhoneNumber = phoneNumber;
             Address = address;
+            Description = description;
         }
 
 
--- ViewModels/ProviderListViewModel.cs
+++ ViewModels/ProviderListViewModel.cs
@@ -6,27 +6,23 @@ using System.Threading.Tasks;
 
 namespace Search4Support.ViewModels
 {
-    public class ProviderDetailViewModel
+    public class ProviderListViewModel
     {
         public int ProviderId { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public string Description { get; set; }
-        public List<Service> Services { get; set; }
-        
 
-        
-        public ProviderDetailViewModel(Provider theProvider)
+
+
+
+
+        public ProviderListViewModel(Provider theProvider)
         {
             ProviderId = theProvider.Id;
             Name = theProvider.Name;
             PhoneNumber = theProvider.PhoneNumber;
             Address = theProvider.Address;
-            Description = theProvider.Description;
-            Services = theProvider.Services;
         }
-
-
     }
 }
