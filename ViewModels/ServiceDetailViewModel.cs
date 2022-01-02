064ecda8190a6fce747ac4615fba473a6e5b17c0
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2022-01-02 17:06:19 -0600

    Added link to Provider Detail in Service Detail

--- ViewModels/ServiceDetailViewModel.cs
+++ ViewModels/ServiceDetailViewModel.cs
@@ -12,6 +12,7 @@ namespace Search4Support.ViewModels
         public int ServiceId { get; set; }
         public string Name { get; set; }
         public string ProviderName { get; set; }
+        public int ProviderId { get; set; }
         public string CategoryName { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
@@ -21,6 +22,7 @@ namespace Search4Support.ViewModels
         {
             ServiceId = theService.Id;
             Name = theService.Name;
+            ProviderId = theService.ProviderId;
             ProviderName = theService.Provider.Name;
             CategoryName = theService.Category.Name;
             Address = theService.Provider.Address;
