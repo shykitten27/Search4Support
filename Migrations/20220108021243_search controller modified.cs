9188859f34cacaf620fcae36e3c351644ad164d8
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2022-01-07 20:14:48 -0600

    search controller updated

---
+++ Migrations/20220108021243_search controller modified.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Search4Support.Migrations
+{
+    public partial class searchcontrollermodified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProviderServices");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProviderServices",
+                columns: table => new
+                {
+                    ProviderId = table.Column<int>(type: "int", nullable: false),
+                    ServiceId = table.Column<int>(type: "int", nullable: false),
+                    ServiceName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProviderServices", x => new { x.ProviderId, x.ServiceId });
+                    table.ForeignKey(
+                        name: "FK_ProviderServices_Providers_ProviderId",
+                        column: x => x.ProviderId,
+                        principalTable: "Providers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProviderServices_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProviderServices_ServiceId",
+                table: "ProviderServices",
+                column: "ServiceId");
+        }
+    }
+}
