b63fb8c24e834c8bdde340ed55253e241ba247d7
Donna Seidel <dseidel01@gmail.com>
2022-01-16 16:06:26 -0600

    add category feature

---
+++ ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Search4Support.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 20 characters long")]
+        public string Name { get; set; }
+
+        public AddCategoryViewModel()
+        {
+            //empty constructor
+        }
+    }
+}
