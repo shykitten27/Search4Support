3f48cf563b1da30c52c9c40454c589d92857bfd1
Donna Seidel <dseidel01@gmail.com>
2022-01-17 00:44:11 -0600

    add view model and update index to show add for authorized users and controller to add and process form to add new provider

--- Controllers/ProvidersController.cs
+++ Controllers/ProvidersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Search4Support.Data;
 using Search4Support.Models;
@@ -11,6 +13,7 @@ using X.PagedList;
 
 namespace Search4Support.Controllers
 {
+    [Authorize]
     public class ProvidersController : Controller
     {
         private ServiceDbContext context;
@@ -20,6 +23,7 @@ namespace Search4Support.Controllers
             context = dbContext;
         }
 
+        [AllowAnonymous]
         // GET: ProvidersController
         public IActionResult Index(string sortOrder, int? page)
         {
@@ -46,14 +50,14 @@ namespace Search4Support.Controllers
 
         }
 
-
+        [AllowAnonymous]
         // GET: ProvidersController/Details/5
         public IActionResult Details(int id)
         {
             return View();
         }
 
-        
+        [AllowAnonymous]
         public IActionResult Detail(int id)
         {
             
@@ -65,6 +69,36 @@ namespace Search4Support.Controllers
             ProviderDetailViewModel viewModel = new ProviderDetailViewModel(theProvider);
             return View(viewModel);    
 
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddProviderViewModel addProviderViewModel = new AddProviderViewModel();
+            return View(addProviderViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessAddProviderForm(AddProviderViewModel addProviderViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Provider newProvider = new Provider
+                {
+                    Name = addProviderViewModel.Name,
+                    PhoneNumber = addProviderViewModel.PhoneNumber,
+                    Address = addProviderViewModel.Address,
+                    //MapLink = addProviderViewModel.MapLink,
+                    Description = addProviderViewModel.Description
+                };
+
+                context.Providers.Add(newProvider);
+                context.SaveChanges();
+
+                return Redirect("/Categories");
+            }
+
+            return View("Add", addProviderViewModel);
         }
     }
 }
